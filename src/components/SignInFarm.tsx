033b92f63d869d1df97354dd3b02e9e91a209317
Rashmi V Abbigeri <rashmiabbigeri@gmail.com>
2023-04-04 03:59:53 +0530

    fix build errors

--- src/components/SignInFarm.tsx
+++ src/components/SignInFarm.tsx
@@ -2,8 +2,12 @@
 import { Box, Button, Divider, Typography } from '@mui/material';
 import { useRouter } from 'next/router';
 
-import { useMutationLinkToGithub, useMutationLinkToGoogle, useMutationLinkToOrcid } from '@/hooks/query/mutation';
-import { particleEthersSigner } from '@/utils/particleInit';
+import {
+  useMutationLinkToGithub,
+  useMutationLinkToGoogle,
+  useMutationLinkToOrcid
+} from '@/hooks/query/mutation';
+import { pn } from '@/utils/particleNetworkAuth';
 
 import ImageButton from './ImageButton';
 import PageGradient from './PageGradient';
@@ -30,20 +34,29 @@ export default function SignInFarm() {
           src='/assets/images/verseLogo.png'
         />
 
-<Button 
-  onClick={() => {particleEthersSigner.connect();}}
->
-          <Typography 
+        <Button
+          onClick={() => {
+            pn.auth.login({
+              // when set social login auth type, will open thirdparty auth page directly.
+              preferredAuthType: 'google',
+              // when set email/phone account and preferredAuthType is email or phone,
+              // Particle Auth will enter directly input verification code page.
+              // when set JWT value and preferredAuthType is jwt, Particle Auth will auto login.
+              loginFormMode: true, // login form mode will hide others ui except login form. only support supportAuthTypes equals email or phone.
+              socialLoginPrompt: 'select_account', //social login prompt.  none | consent | select_account
+            });
+          }}
+        >
+          <Typography
             sx={{
               fontSize: 12,
               color: '#957C9D',
               mt: 4,
             }}
           >
-            Connect Wallet
+            Connect Wallet with PN
           </Typography>
-
-</Button>
+        </Button>
 
         <Typography
           variant='h4'
